918db0bf257d9a4b4f675cec2accf48a9e4b91c1
Gopalakrishnan <krrish.sgk@gmail.com>
2015-08-18 20:29:10 +0530

    made code code readable
    deleted a few lines of needless stuff

--- GettingitTogether - 3003.R
+++ GettingitTogether - 3003.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(dplyr)
 
-eplurl <- "http://web.archive.org/web/20140704090053/http://www.bbc.com/sport/football/premier-league/results"
+link1314 <- "http://web.archive.org/web/20140704090053/http://www.bbc.com/sport/football/premier-league/results"
 tempdf <- data.frame()
 
 tempdf <- do.call("rbind", apply(epllinks,MARGIN=2,getmatchdata))
@@ -11,27 +11,17 @@ tempdf <- rbindlist(epllinks$matchlinks[1],use.names=FALSE,fill=FALSE)
 epllinks <- leaguelinks(link1314)
 epllinks <- epllinks[1:320,]
 #subsetting epllinks to experiment
-<<<<<<< HEAD
 templinks <- epllinks[1:190,]
 
 #Run the data.table package before using rbindlist
-finaldf <- lapply(templinks$matchlinks,FUN=getmatchdata) 
-
-
-%>%
-=======
-templinks <- epllinks[3,]
-
-#Run the data.table package before using rbindlist
-shots1314 <- lapply(templinks$matchlinks,FUN=getmatchdata) %>%
->>>>>>> origin/master
+shots1314 <- lapply(epllinks$matchlinks,FUN=getmatchdata) %>%
     rbindlist(.) %>%
     makedata(.)
 #merging data with fixtures and score
 shots1314 <- merge(epllinks, shots1314, by.x="matchlinks", by.y="url")
 
 #creating dataset for shots alone
-shotsData <- filter(.data=newDataFrame,eventType=="shot")
+shots1314 <- filter(.data=shots1314,eventType=="shot")
 
 shotsNas <- filter(.data=shotsData,is.na(shotsData$shotposition))
 
