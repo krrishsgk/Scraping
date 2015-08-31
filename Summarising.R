43203dd745d5d0222327d20e0b482f664c77de10
krrishsgk <krrish.sgk@gmail.com>
2015-08-31 21:52:32 +0530

    added summarising code
    summarised by player and player, shot, position

--- Summarising.R
+++ Summarising.R
@@ -11,7 +11,9 @@ library(dplyr)
 
 #Checking the data for cleanliness
 temp <- grep("Assisted", shots1415$primaryplayer, perl = TRUE)
-dump <- shots1415[temp,]
+dump <- shots1315[temp,]
+
+#Grouping by player, shot location, and foot
 byPlayer <- group_by(shots1315, primaryplayer, shotposition, foot)
 byPlayer <-summarise(byPlayer, shots = n())
 
@@ -21,5 +23,40 @@ SoT <- group_by(SoT, primaryplayer,  shotposition, foot)
 SoT <- summarise(SoT, shots = n())
 SoT <- rename(.data = SoT, onTarget = shots)
 
-finaldataset <- merge(byPlayer, SoT, by = c("primaryplayer", "shotposition","foot"), all.x = TRUE)
+tempdata <- merge(byPlayer, SoT, by = c("primaryplayer", "shotposition","foot"), all.x = TRUE)
 
+
+#Filtering for goals
+goals <- filter(shots1315, isgoal == TRUE)
+goals <- group_by(goals, primaryplayer, shotposition, foot)
+goals <- summarise(goals, shots = n())
+goals <- rename(.data = goals, goals = shots)
+
+finaldataset <- merge(tempdata, goals, by = c("primaryplayer", "shotposition","foot"), all.x = TRUE)
+
+#Creating Ratios
+finaldataset <- mutate(finaldataset, ShootingAcc = (onTarget/shots)*100, Conversion = (goals/onTarget)*100)
+
+#For players only
+#Grouping shots
+onlyPlayer <-  group_by(shots1315, primaryplayer)
+onlyPlayer <- summarise(onlyPlayer, shots = n())
+
+playerSoT <- filter(shots1315, goalposition != "NA")
+playerSoT <- group_by(playerSoT, primaryplayer)
+playerSoT <- summarise(playerSoT, shots = n())
+playerSoT <- rename(.data = playerSoT, onTarget = shots)
+
+playertempdata <- merge(onlyPlayer, playerSoT, by = c("primaryplayer"), all.x = TRUE)
+
+
+#Filtering for goals
+playergoals <- filter(shots1315, isgoal == TRUE)
+playergoals <- group_by(playergoals, primaryplayer)
+playergoals <- summarise(playergoals, shots = n())
+playergoals <- rename(.data = playergoals, goals = shots)
+
+playerfinaldataset <- merge(playertempdata, playergoals, by = c("primaryplayer"), all.x = TRUE)
+
+#Creating Ratios
+playerfinaldataset <- mutate(playerfinaldataset, ShootingAcc = (onTarget/shots)*100, Conversion = (goals/onTarget)*100)
