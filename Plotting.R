8c76b3069876ec5a03cf3c96acf5f64aff37efe7
Gopalakrishnan <krrish.sgk@gmail.com>
2015-09-21 21:16:55 +0530

    added coordinates

--- Plotting.R
+++ Plotting.R
@@ -8,7 +8,7 @@ unitedgame <- unitedgame[which(unitedgame$eventType == "shot"),]
 
 
 #Take each shot, assign it random coordinates based on its zone
-shottypes <- c("free kick","Penalty|penalty","very close range","centre of the box","outside the box","a difficult angle on the right",
+shottypes <- c("very close range","centre of the box","outside the box","a difficult angle on the right",
                "a difficult angle on the left","left side of the box","right side of the box",
                "right side of the six yard box","left side of the six yard box",
                "from a difficult angle and long range on the right",
@@ -16,12 +16,14 @@ shottypes <- c("free kick","Penalty|penalty","very close range","centre of the b
                "long range on the left","long range on the right",
                "from more than 40 yards on the right wing","from more than 40 yards on the left wing")
 
-shotForms <- c(c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),
-               c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),
-               c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),
-               c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),c(runif(1),runif(1)),
-               c(runif(1),runif(1)),c(runif(1),runif(1)))
+shotCoords <- c(runif(1,min=0,max=6),runif(1,min=6,max=17),runif(1,min=17,max=30),runif(1,min=0,max=17),
+               runif(1,min=0,max=17),runif(1,min=0,max=17),runif(1,min=0,max=17),runif(1,min=0,max=6),
+               runif(1,min=0,max=6),runif(1,min=17,max=30),runif(1,min=17,max=30),runif(1,min=30,max=38.67),
+               runif(1,min=30,max=38.67),runif(1,min=30,max=38.67),runif(1,min=38.67,max=52),runif(1,min=38.67,max=52))
 
 for (i in 1:length(shottypes)) {
-  unitedgame$coordinates[grepl(shottypes[i],unitedgame$livetext, perl=TRUE)] <- shotForms[i]
+  unitedgame$coordinatesX[grepl(shottypes[i],unitedgame$livetext, perl=TRUE)] <- shotCoords[i]
 }
+
+
+switch(EXPR=unitedgame$shotposition
