97a86d4c02ce95d5750b10c135babbb001e65930
Gopalakrishnan <krrish.sgk@gmail.com>
2015-09-23 22:45:48 +0530

    wrote a function to a assign coordinates

--- Plotting.R
+++ Plotting.R
@@ -16,14 +16,32 @@ shottypes <- c("very close range","centre of the box","outside the box","a diffi
                "long range on the left","long range on the right",
                "from more than 40 yards on the right wing","from more than 40 yards on the left wing")
 
-shotCoords <- c(runif(1,min=0,max=6),runif(1,min=6,max=17),runif(1,min=17,max=30),runif(1,min=0,max=17),
-               runif(1,min=0,max=17),runif(1,min=0,max=17),runif(1,min=0,max=17),runif(1,min=0,max=6),
-               runif(1,min=0,max=6),runif(1,min=17,max=30),runif(1,min=17,max=30),runif(1,min=30,max=38.67),
-               runif(1,min=30,max=38.67),runif(1,min=30,max=38.67),runif(1,min=38.67,max=52),runif(1,min=38.67,max=52))
 
-for (i in 1:length(shottypes)) {
-  unitedgame$coordinatesX[grepl(shottypes[i],unitedgame$livetext, perl=TRUE)] <- shotCoords[i]
+xCoordFunc <- function(shotPos)
+{
+    switch(shotPos,
+           "CloseRange" = runif(1,min=0,max=6),
+           "CBox" = runif(1,min=6,max=17),
+           "OBox" = runif(1,min=17,max=30),
+           "DiffAngR" = runif(1,min=0,max=17),
+           "DiffAngL" =  runif(1,min=0,max=17),
+           "LSBox" = runif(1,min=0,max=17),
+           "RSBox" =  runif(1,min=0,max=17),
+           "RS6Box" = runif(1,min=0,max=6),
+           "LS6Box" = runif(1,min=0,max=6),
+           "DiffAngLongR" = runif(1,min=17,max=30),
+           "DiffAngLongL" = runif(1,min=17,max=30),
+           "35YardsPlus" = runif(1,min=30,max=38.67),
+           "LongL" = runif(1,min=30,max=38.67),
+           "LongR" = runif(1,min=30,max=38.67),
+           "40PlusR" = runif(1,min=38.67,max=52),
+           "40PlusL" = runif(1,min=38.67,max=52)
+           )
 }
 
+unitedgame$xCord <- lapply(unitedgame$shotposition,FUN=xCoordFunc)
 
-switch(EXPR=unitedgame$shotposition
+
+xy <- matrix(runif(20), ncol = 2)  # a matrix with some arbitrary points as coordinates..
+xy.sp <- SpatialPoints(xy)  # ..converted into a spatial object
+plot(xy.sp, pch = 2)
